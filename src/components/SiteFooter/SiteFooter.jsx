24660304d7b29207954980df465664c211602400
Sebbybobbler <seb_davies@hotmail.co.uk>
2024-08-13 14:47:07 +0100

    add a footer and remove header

--- src/components/SiteFooter/SiteFooter.jsx
+++ src/components/SiteFooter/SiteFooter.jsx
@@ -1,5 +1,13 @@
+import styles from "./SiteFooter.module.css";
+
 function SiteFooter() {
-  return <div>SiteFooter</div>;
+    return (
+        <>
+            <div className={styles.titleDiv}>
+                <p>Seb Davies 2024</p>
+            </div>
+        </>
+    );
 }
 
 export default SiteFooter;
--- src/components/SiteHeader/SiteHeader.jsx
+++ src/components/SiteHeader/SiteHeader.jsx
@@ -1,5 +1,5 @@
 function SiteHeader() {
-  return <div>SiteHeader</div>;
+    return <div></div>;
 }
 
 export default SiteHeader;
